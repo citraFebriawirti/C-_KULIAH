1fb5421b2ee196ac7153ae008a60417605c94a39
Citra Febriawirti <83469523+citraFebriawirti@users.noreply.github.com>
2021-06-02 09:39:45 +0700

    Add files via upload

--- 3_utss.cpp
+++ 3_utss.cpp
@@ -1,33 +1,37 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main(){
   string nama = "citra febriawirti" ;
-  int lama_menginap = 15 ;
-  int tarif_kamar = 50000 ;
-  float  total_bayar;
+  int lama_menginap ;
+  int tarif_kamar  ;
+  int  total_bayar;
+  string  bonus ;
+
+
+cout<<"nama saya adalah "<<nama<<endl;
+cout<<"lama inap adalah : " ;
+cin>>lama_menginap ;
+cout<<"tarif kamar : ";
+cin>>tarif_kamar;
 
   total_bayar = lama_menginap*tarif_kamar;
 
-
-  string  bonus ;
-
   if( total_bayar > 500000 ){
-	   bonus = "Voucher KFC senilai 100000";
-	   }  else if (total_bayar > 400000) {
-				   bonus = "Voucher Pulsa senilai 500000";
-				   }else if ( total_bayar < 400000) {
-					   bonus = "tidak mendapatkan bonus ";
-				   }
+	   bonus = "Voucher KFC senilai 100.000";
+  }else if (total_bayar > 400000) {
+	   bonus = "Voucher Pulsa senilai 50.000";
+  }else  {
+		 bonus = "tidak mendapatkan bonus ";}
 
-cout<<"nama saya adalah "<<nama;
-cout<<"lama inap adalah : ";cin>>lama_menginap ;
-cout<<"Total bayar adalah : ";<<total_bayar;
-cout<<"bonus yang didapatkan adalah : ";<<bonus<<endl;
+cout<<"Total bayar adalah : "<<total_bayar;
+cout<<"\n bonus yang didapatkan adalah : "<<bonus;
 
 getchar();
 getchar();
 return 0;
+
 
 
 }
---
+++ versi_3uts.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string nama, bonus;
+	int lama, tarif, total;
+
+	cout<<"Nama tamu            :"; cin>>nama;
+	cout<<"Lama Nginap (hari)   :"; cin>>lama;
+	cout<<"Tarif kamar          :"; cin>>tarif;
+	total = lama * tarif;
+
+	if (total>500000) {
+		bonus="Voucer KFC";
+	}
+	else if(total>400000){
+		 bonus="Voucher Pulsa";
+	}
+	else {
+		bonus="Tidak ada bonus";
+    }
+   cout<<"Total bayar           :"<<total;
+   cout<<"\n Bonusnya adalah    :"<<bonus;
+   getchar();
+   getchar();
+   return 0 ;
+}
